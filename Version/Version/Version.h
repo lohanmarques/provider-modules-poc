ad041bca929b3a538cdea5018ab3fcd321187f09
gabriel909 <gabriel90939@gmail.com>
2020-12-18 17:38:30 -0300

    Criando Version

---
+++ Version/Version/Version.h
@@ -0,0 +1,19 @@
+//
+//  Version.h
+//  Version
+//
+//  Created by Gabriel Oliveira on 18/12/20.
+//  Copyright © 2020 Gabriel Oliveira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Version.
+FOUNDATION_EXPORT double VersionVersionNumber;
+
+//! Project version string for Version.
+FOUNDATION_EXPORT const unsigned char VersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Version/PublicHeader.h>
+
+
